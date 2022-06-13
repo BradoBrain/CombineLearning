6d98d9e7b473b7660ae220a62a39b9cafd3f0966
Brado Brain <vinogradov.artem@outlook.com>
2022-06-13 11:12:39 +0300

    Initial Commit

---
+++ CombineLearning/CombineLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineLearningApp.swift
+//  CombineLearning
+//
+//  Created by Artem Vinogradov on 13.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CombineLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineLearning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineLearning
+//
+//  Created by Artem Vinogradov on 13.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
